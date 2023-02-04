75f079b9a667047633f39cb7aea11086193eac75
aadyagilev <dthmth@gmail.com>
2023-02-04 18:57:31 +0500

    add new query

--- CLOB.sql
+++ CLOB.sql
@@ -1,1 +1,3 @@
-select * from sys.dba_segments where segment_type = 'LOBSEGMENT' and owner = 'OP_USER';
+select * from sys.dba_segments where segment_type = 'LOBSEGMENT' and owner = 'OP_USER'
+minus 
+select * from sys.dba_segments where segment_type != 'LOBSEGMENT' and owner != 'OP_USER'
