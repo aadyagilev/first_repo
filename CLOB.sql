c27caf06b637d5e9bc8b28afe5d66a14f3222be0
aadyagilev <dthmth@gmail.com>
2023-02-04 19:03:41 +0500

    very imortant PUPA Added

--- CLOB.sql
+++ CLOB.sql
@@ -1,3 +1,5 @@
 select * from sys.dba_segments where segment_type = 'LOBSEGMENT' and owner = 'OP_USER'
 minus 
 select * from sys.dba_segments where segment_type != 'LOBSEGMENT' and owner != 'OP_USER'
+;
+select "PUPA" from dual;
